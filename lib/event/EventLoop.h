b466cb6347eb324c920f8f9b60c068070c697891
Jan Erik Hanssen <jhanssen@gmail.com>
2018-05-26 01:11:29 -0700

    initial commit

---
+++ lib/event/EventLoop.h
@@ -0,0 +1,16 @@
+#ifndef EVENTLOOP_H
+#define EVENTLOOP_H
+
+#include <config.h>
+
+namespace reckoning {
+namespace event {
+
+class EventLoop
+{
+public:
+};
+
+}} // namespace reckoning::event
+
+#endif
