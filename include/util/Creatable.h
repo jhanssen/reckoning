878ac785897a2f04296b526c4b5c585139ec8ad5
Jan Erik Hanssen <jhanssen@gmail.com>
2020-03-02 16:54:28 -0800

    hey

--- include/util/Creatable.h
+++ include/util/Creatable.h
@@ -2,9 +2,16 @@
 #define CREATABLE_H
 
 #include <memory>
+#include <util/IsDetected.h>
 
 namespace reckoning {
 namespace util {
+
+template<typename T>
+using init_t = decltype( std::declval<T&>().init() );
+
+template<typename T>
+constexpr bool has_init = is_detected<init_t, T>::value;
 
 template <typename T>
 class Creatable
@@ -22,13 +29,9 @@ public:
             }
         };
 
-        constexpr bool hasInit = requires(const T& t) {
-            t.init();
-        };
-
         auto ptr = std::make_shared<EnableMakeShared>(std::forward<Args>(args)...);
 
-        if (hasInit) {
+        if constexpr (has_init<T>) {
             ptr->init();
         }
 
